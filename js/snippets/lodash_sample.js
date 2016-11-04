283fe4000a82a38126b1638d38e63d95305d2366
Kent Lee <ikenticus@gmail.com>
2016-11-03 22:34:12 -0400

    adding clone samples

--- js/snippets/lodash_sample.js
+++ js/snippets/lodash_sample.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 
 var data = JSON.parse(fs.readFileSync(process.argv[1] + 'on').toString());
 
+console.log('\nLIST forEach');
 console.log('===== basic');
 data.feed['league-content']['season-content']['conference-content'].forEach(function(c) {
 	console.log(c.conference.id);
@@ -18,6 +19,7 @@ _.forEach(data.feed['league-content']['season-content']['conference-content'], (
 	});
 });
 
+console.log('\nDICT forEach');
 console.log('===== basic');
 var name = data.feed['league-content'].league.name[1];
 Object.keys(name).forEach(function(m) {
@@ -48,6 +50,7 @@ function bindRoutes (handlers, api) {
 }
 */
 
+console.log('\nMAP');
 var out = {};
 data = { one: 1, two: 2, three: 3, four: 4, five: 5 };
 console.log('===== basic');
@@ -60,5 +63,97 @@ _.map(data, (key) => {
     if (data[key] % 2) out[key] = data[key] * 10;
 });
 console.log(out);
+
+
+console.log('\n\n\n\n\n\n\n\n\nSTART')
+data = {
+    //ELEM: [ 1, 2, 3, 4 ],
+    //MIDDLE: [ 5, 6, 7, 8 ],
+    HIGH: null,
+    COLLEGE: {
+        Freshman: {
+            Math: { Calculus: [ 'AB', 'BC' ] }
+        },
+        Sophomore: {
+            Math: {
+                Algebra: [ 'I', 'II' ],
+                Geometry: [ 1, 2, 3, 4 ],
+                Trigonometry: [ 1, 2, 3 ]
+            }
+        },
+        Junior: {},
+        Senior: {}
+    }
+}
+var orig = _.cloneDeep(data);
+console.log('===== orig');
+console.log(orig);
+
+console.log('\n\n\nCLONE equal');
+var equal = orig;
+console.log('-- COLLEGE -> HIGH');
+equal.HIGH = equal.COLLEGE
+console.log('===== equal');
+console.log(equal);
+console.log('===== orig');
+console.log(orig);
+console.log('-- Sophomore -> Junior');
+equal.COLLEGE.Junior = equal.COLLEGE.Sophomore;
+console.log('===== equal');
+console.log(equal);
+console.log('===== orig');
+console.log(orig);
+console.log('-- delete College Sophomore Geometry');
+delete equal.COLLEGE.Sophomore.Math.Geometry;
+console.log('===== equal');
+console.log(JSON.stringify(equal, null, 4));
+console.log('===== orig');
+console.log(JSON.stringify(orig, null, 4));
+
+
+console.log('\n\n\nCLONE shallow');
+orig = _.cloneDeep(data);
+console.log('-- COLLEGE -> HIGH');
+var shallow = _.clone(orig);
+shallow.HIGH = _.clone(shallow.COLLEGE);
+console.log('===== shallow');
+console.log(shallow);
+console.log('===== orig');
+console.log(orig);
+console.log('-- Sophomore -> Junior');
+shallow.COLLEGE.Junior = _.clone(shallow.COLLEGE.Sophomore);
+console.log('===== shallow');
+console.log(shallow);
+console.log('===== orig');
+console.log(orig);
+console.log('-- delete College Sophomore Geometry');
+delete shallow.COLLEGE.Sophomore.Math.Geometry;
+console.log('===== shallow');
+console.log(JSON.stringify(shallow, null, 4));
+console.log('===== orig');
+console.log(JSON.stringify(orig, null, 4));
+
+
+console.log('\n\n\nCLONE deep');
+orig = _.cloneDeep(data);
+console.log('-- COLLEGE -> HIGH');
+var deep = _.cloneDeep(orig);
+deep.HIGH = _.cloneDeep(deep.COLLEGE);
+console.log('===== deep');
+console.log(deep);
+console.log('===== orig');
+console.log(orig);
+console.log('-- Sophomore -> Junior');
+deep.COLLEGE.Junior = _.cloneDeep(deep.COLLEGE.Sophomore);
+console.log('===== deep');
+console.log(deep);
+console.log('===== orig');
+console.log(orig);
+console.log('-- delete College Sophomore Geometry');
+delete deep.COLLEGE.Sophomore.Math.Geometry;
+console.log('===== deep');
+console.log(JSON.stringify(shallow, null, 4));
+console.log('===== orig');
+console.log(JSON.stringify(orig, null, 4));
 
 
