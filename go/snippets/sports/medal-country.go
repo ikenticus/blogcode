cbab516719b562825751a7ab94639ba7de30b66a
ikenticus <ikenticus@gmail.com>
2018-02-12 15:51:49 -0500

    adding infostrada read.json format

--- go/snippets/sports/medal-country.go
+++ go/snippets/sports/medal-country.go
@@ -17,6 +17,8 @@ import (
 const (
     apiPath = "http://%s/svc/Games_v2.svc/json/%s?languageCode=2&competitionSetId=%d&season=%s"
     execPath = "/exec/crawl/infostrada?paths=Games_v2.svc/json/%s&filters=languageCode=2,competitionSetId=%d,season=%s,nocId=%d\n"
+    queryString = "languageCode=2&competitionSetId=%d&season=%s&nocId=%d"
+    module = "Games_v2"
 )
 
 var (
@@ -31,6 +33,17 @@ var (
         "noc": "GetMedalTableNOCDetail_Season",
     }
 )
+
+// curl read.json | gojson -name=ReadData, then breaking apart
+type InfoFeed struct {
+    Method       string `json:"c_Method"`
+    Module       string `json:"c_Module"`
+    QueryString  string `json:"c_QueryString"`
+}
+
+type InfoData struct {
+	Data []InfoFeed `json:"data"`
+}
 
 // curl methods[all] | gojson -name=NOCList
 type NOCList []struct {
@@ -123,7 +136,16 @@ func printList (domain string, out string) {
             fmt.Printf(execPath, method, gtype, season, n)
         }
     case "read":
-
+        var data InfoData
+        var feed InfoFeed
+        for _, n := range nocIds {
+            feed.Method = method
+            feed.Module = module
+            feed.QueryString = fmt.Sprintf(queryString, gtype, season, n)
+            data.Data = append(data.Data, feed)
+        }
+        output, _ := json.MarshalIndent(data, "", "    ")
+        fmt.Println(string(output))
     default:
         fmt.Printf("NOCS: %v\n", nocIds)
     }
