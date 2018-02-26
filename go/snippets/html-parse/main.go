697d0c8c0610a68b6511cafdf16c93761b680cb6
ikenticus <ikenticus@gmail.com>
2018-02-26 18:56:25 -0500

    first iteration of html-parse using parse

--- go/snippets/html-parse/main.go
+++ go/snippets/html-parse/main.go
@@ -23,16 +23,27 @@ func main() {
 
     doc, _ := html.Parse(strings.NewReader(string(input)))
     var f func(*html.Node)
+    start := false
     f = func(n *html.Node) {
         //fmt.Println(n.Type, n.Attr, n.Data, n.DataAtom)
-        if n.Type == html.ElementNode {
-            if n.Data == "a" {
+        switch n.Type {
+        case html.ElementNode:
+            if n.Data == "tr" {
                 for _, a := range n.Attr {
-                    if a.Key == "href" {
-                        fmt.Println(a.Val)
+                    if a.Key == "class" && a.Val == "stathead" {
+                        if start {
+                            start = false
+                        } else {
+                            start = true
+                        }
+                        fmt.Println(n.Data, start)
                         break
                     }
                 }
+            }
+        case html.TextNode:
+            if start {
+                fmt.Printf("%q\n", n.Data) 
             }
         }
     	for c := n.FirstChild; c != nil; c = c.NextSibling {
