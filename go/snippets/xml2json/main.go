63139c6c20fb51238ef5c12d2988308af9b1c94b
ikenticus <ikenticus@gmail.com>
2018-06-26 16:22:03 -0400

    adding xml2json logic

---
+++ go/snippets/xml2json/main.go
@@ -0,0 +1,34 @@
+
+package main
+
+import (
+    "encoding/json"
+	"fmt"
+	"io/ioutil"
+    "os"
+    "strings"
+
+	xj "github.com/basgys/goxml2json"
+)
+
+func main() {
+	source := os.Args[1]
+	input, err := ioutil.ReadFile(source)
+	if err != nil {
+		fmt.Errorf("failed to read json file %q: %v", source, err)
+		os.Exit(10)
+	}
+
+	data := strings.NewReader(string(input))
+	output, err := xj.Convert(data)
+	if err != nil {
+		fmt.Errorf("failed to convert %s to json: %v", source, err)
+		os.Exit(20)
+	}
+	fmt.Println(output.String())
+
+    var clean interface{}
+    json.Unmarshal(output.Bytes(), &clean)
+    pretty, _ := json.MarshalIndent(clean, "", "    ")
+    fmt.Println(string(pretty))
+}
