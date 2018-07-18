07680b816c42f2c61a14524aed54b4bd893c43a4
ikenticus <ikenticus@gmail.com>
2018-07-18 19:57:37 -0400

    adding initial asset-check

---
+++ go/snippets/asset-check/helpers/yaml.go
@@ -0,0 +1,96 @@
+package helpers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+
+	vaultApi "github.com/hashicorp/vault/api"
+	strcase "github.com/stoewer/go-strcase"
+	yaml "gopkg.in/yaml.v2"
+)
+
+const debug = false
+
+type URL struct {
+	API     string
+	GraphQL string
+}
+
+type Config struct {
+	APIKey   string `env:"apigeeconsumerkey"`
+	Front    string
+	Sitecode string
+	URL      URL
+}
+
+func getVaultField(vaultPath string, field string) string {
+	vaultConfig := vaultApi.DefaultConfig()
+	vaultClient, err := vaultApi.NewClient(vaultConfig)
+	if err != nil {
+		fmt.Printf("An error occurred creating vaultClient: %v\n", err)
+		return ""
+	}
+	dbConfig, err := vaultClient.Logical().Read(vaultPath)
+	if err != nil {
+		fmt.Printf("An error occurred reading secret: %v\n", err)
+		return ""
+	}
+	return dbConfig.Data[field].(string)
+}
+
+func initConfig(config Config) Config {
+	k := reflect.ValueOf(&config)
+	v := reflect.ValueOf(config)
+
+	for i := 0; i < v.NumField(); i++ {
+		// Need to figure out how to recurse
+		switch v.Field(i).Kind() {
+		case reflect.String:
+			if strings.HasPrefix(v.Field(i).String(), "VAULT") {
+				vv := getVaultField(
+					strings.Split(v.Field(i).String(), ".")[1],
+					strcase.LowerCamelCase(k.Elem().Type().Field(i).Tag.Get("env")),
+				)
+				if debug {
+					fmt.Println("\t", strcase.LowerCamelCase(k.Elem().Type().Field(i).Tag.Get("env")))
+					fmt.Println("\t", strings.Split(v.Field(i).String(), ".")[1])
+					fmt.Println("\t", vv)
+				}
+				k.Elem().Field(i).SetString(vv)
+			}
+		}
+	}
+	return config
+}
+
+// readYAML will read and unmarshal YAML file
+func readYAML(yamlFile string) Config {
+	var config Config
+	data, err := ioutil.ReadFile(yamlFile)
+	if err == nil {
+		err = yaml.Unmarshal(data, &config)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Printf("Loaded configuration from: %s\n", yamlFile)
+	return config
+}
+
+func Yaml(yamlFile string) Config {
+	var config Config
+	yamlPath, err := filepath.Abs(yamlFile)
+	if err != nil {
+		fmt.Printf("Input YAML %s error: %v", yamlFile, err)
+		os.Exit(2)
+	}
+	if _, err := os.Stat(yamlPath); err == nil {
+		config = initConfig(readYAML(yamlPath))
+	}
+	return config
+}
---
+++ go/snippets/asset-check/main.go
@@ -0,0 +1,20 @@
+// asset-check compares API to GraphQL assets
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/ikenticus/blogcode/go/snippets/asset-check/helpers"
+)
+
+func main() {
+	if len(os.Args) > 1 {
+		config := helpers.Yaml(os.Args[1])
+		fmt.Println(config)
+		//helpers.Check(config)
+	} else {
+		fmt.Printf("Usage: %s <config>.yaml\n", path.Base(os.Args[0]))
+	}
+}
