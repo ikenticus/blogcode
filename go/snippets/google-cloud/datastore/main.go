e0c7f11771883054bc4194cfe9b34a59a52d397f
ikenticus <ikenticus@gmail.com>
2018-08-10 15:22:26 -0400

    adding google cloud datastore/pubsub snippets

---
+++ go/snippets/google-cloud/datastore/main.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"strings"
+	"time"
+	"unicode/utf8"
+
+	"cloud.google.com/go/datastore"
+)
+
+// struct for Datastore Kind
+type DatastoreKind struct {
+	KindName            string    `datastore:"kind_name"`
+	EntityBytes         int       `datastore:"entity_bytes"`
+	BuiltinIndexBytes   int       `datastore:"builtin_index_bytes"`
+	BuiltinIndexCount   int       `datastore:"builtin_index_count"`
+	CompositeIndexBytes int       `datastore:"composite_index_bytes"`
+	CompositeIndexCount int       `datastore:"composite_index_count"`
+	Timestamp           time.Time `datastore:"timestamp"`
+	Count               int       `datastore:"count"`
+	Bytes               int       `datastore:"bytes"`
+}
+
+func listKinds(ctx context.Context, client *datastore.Client) {
+	query := datastore.NewQuery("__Stat_Kind__").Order("kind_name")
+
+	kinds := []*DatastoreKind{}
+	_, err := client.GetAll(ctx, query, &kinds)
+
+	if err != nil {
+		fmt.Errorf("Failed to run Kinds query\n")
+	}
+	if len(kinds) == 0 {
+		fmt.Println("\nNo Kinds found!")
+	}
+
+	for _, k := range kinds {
+		fmt.Printf("\nKind: %s\n  Count: %d\n  Bytes: %d\n", k.KindName, k.Count, k.Bytes)
+		listKeys(ctx, client, k.KindName)
+	}
+}
+
+func listKeys(ctx context.Context, client *datastore.Client, key string) {
+	query := datastore.NewQuery(key).KeysOnly().Limit(10)
+
+	keys, err := client.GetAll(ctx, query, nil)
+	if err != nil {
+		fmt.Errorf("\nFailed to run Keys query\n")
+	}
+	if len(keys) == 0 {
+		fmt.Printf("\nNo Keys found for %s!", key)
+	}
+
+	fmt.Printf("\n%s:\n", key)
+	/*
+		if len(keys) > 10 {
+			keys = keys[:10]
+		} // same as .Limit(#) above
+	*/
+	var last string
+	for _, k := range keys {
+		fmt.Printf("  %s\n", k.Name)
+		last = k.Name
+	}
+
+	if !strings.HasPrefix(key, "__") {
+		//fmt.Printf("\nLast: %q, %q\n", last, key)
+		readKey(ctx, client, key, last)
+	}
+}
+
+// struct for Datastore Kind
+type DatastoreTask struct {
+	Category        string
+	Done            bool
+	Priority        float64
+	Description     string `datastore:",noindex"`
+	PercentComplete float64
+	Created         time.Time
+}
+
+func listTasks(ctx context.Context, client *datastore.Client) {
+	query := datastore.NewQuery("Task")
+
+	tasks := []*DatastoreTask{}
+	_, err := client.GetAll(ctx, query, &tasks)
+	if err != nil {
+		fmt.Errorf("\nFailed to run Tasks query\n")
+	}
+	if len(tasks) == 0 {
+		fmt.Println("\nNo Tasks found!")
+	}
+
+	for _, t := range tasks {
+		fmt.Printf("\nTask: %s\n", t.Description)
+	}
+}
+
+type DatastoreEntity struct {
+	Value []byte `datastore:",noindex"`
+}
+
+func readKey(ctx context.Context, client *datastore.Client, kind string, id string) {
+	key := datastore.NameKey(kind, id, nil)
+
+	var entity DatastoreEntity
+	if err := client.Get(ctx, key, &entity); err != nil {
+		if err == datastore.ErrNoSuchEntity {
+			fmt.Errorf("\nEntity not found: %s\n", id)
+		}
+	}
+
+	zip, err := gzip.NewReader(bytes.NewBuffer(entity.Value))
+	if err != nil {
+		fmt.Errorf("failed to initialize gzip Reader for id %q: %v", id, err)
+	}
+
+	out, err := ioutil.ReadAll(zip)
+	if err != nil {
+		fmt.Errorf("failed to decompress gzipped data for id %q: %v", id, err)
+	}
+
+	//fmt.Println(string(out))
+	var data interface{}
+	json.Unmarshal(out, &data)
+	output, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		fmt.Errorf("failed to JSON unmarshal data for id %q: %v", id, err)
+	}
+	fmt.Println(string(output))
+}
+
+// struct for Google key.json data
+type KeyData struct {
+	AuthProviderX509CertURL string  `json:"auth_provider_x509_cert_url"`
+	AuthURI                 string  `json:"auth_uri"`
+	ClientEmail             string  `json:"client_email"`
+	ClientID                float64 `json:"client_id,string"`
+	ClientX509CertURL       string  `json:"client_x509_cert_url"`
+	PrivateKey              string  `json:"private_key"`
+	PrivateKeyID            string  `json:"private_key_id"`
+	ProjectID               string  `json:"project_id"`
+	TokenURI                string  `json:"token_uri"`
+	Type                    string  `json:"type"`
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s key.json\n", path.Base(os.Args[0]))
+		os.Exit(1)
+	}
+	keyPath := os.Args[1]
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", keyPath)
+
+	keyFile, err := ioutil.ReadFile(keyPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var keyData KeyData
+	err = json.Unmarshal(keyFile, &keyData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	projectID := keyData.ProjectID
+	client, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
+		fmt.Errorf("\nFailed to connect to datastore\n")
+	}
+
+	fmt.Printf("\nProject: %s\n%s\n", projectID, strings.Repeat("-", utf8.RuneCountInString(projectID)+10))
+	listKeys(ctx, client, "__kind__")
+	listKinds(ctx, client)
+	listTasks(ctx, client)
+	//readKey(ctx, client, "rawAsset", "107797671")
+}
---
+++ go/snippets/google-cloud/pubsub/main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+
+	"cloud.google.com/go/pubsub"
+	"google.golang.org/api/iterator"
+)
+
+const (
+	maxMessages = 10
+)
+
+// struct for Google key.json data
+type KeyData struct {
+	AuthProviderX509CertURL string  `json:"auth_provider_x509_cert_url"`
+	AuthURI                 string  `json:"auth_uri"`
+	ClientEmail             string  `json:"client_email"`
+	ClientID                float64 `json:"client_id,string"`
+	ClientX509CertURL       string  `json:"client_x509_cert_url"`
+	PrivateKey              string  `json:"private_key"`
+	PrivateKeyID            string  `json:"private_key_id"`
+	ProjectID               string  `json:"project_id"`
+	TokenURI                string  `json:"token_uri"`
+	Type                    string  `json:"type"`
+}
+
+func listSubscriptions(ctx context.Context, client *pubsub.Client, topic string) {
+	// [START pubsub_list_topic_subscriptions]
+	var subs []*pubsub.Subscription
+	it := client.Topic(topic).Subscriptions(ctx)
+	for {
+		sub, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to list subscriptions\n")
+		}
+		fmt.Println(sub)
+		subs = append(subs, sub)
+	}
+	// [END pubsub_list_topic_subscriptions]
+}
+
+func getMessage(ctx context.Context, client *pubsub.Client, sub string) {
+	received := 0
+	s := client.Subscription(sub)
+	cctx, cancel := context.WithCancel(ctx)
+	err := s.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
+		msg.Ack()
+		fmt.Printf("\nMessage: %s\n   Time: %s\n   Attr: %s\n   Data: %s\n",
+			msg.ID, msg.PublishTime, msg.Attributes, msg.Data)
+		/*
+			if readMessage(ctx, msg) {
+				msg.Ack()
+			} else {
+				msg.Nack()
+			}
+		*/
+		received++
+		if received == maxMessages {
+			cancel() // stop after receiving enough messages
+		}
+	})
+	if err != nil {
+		log.Fatalf("Failed to get %s message\n", sub)
+	}
+	//fmt.Println("No Error")
+}
+
+func putMessage(ctx context.Context, client *pubsub.Client, topic string) {
+	// service account may not have permissions to create subscription
+	/*
+		sub, err := client.CreateSubscription(ctx, topic, pubsub.SubscriptionConfig{
+			Topic:       client.Topic(topic),
+			AckDeadline: 20 * time.Second,
+		})
+		if err != nil {
+			log.Fatalf("\nfailed to create subscription for %q: %v", topic, err)
+		}
+		fmt.Printf("Created subscription: %v\n", sub)
+	*/
+	res := client.Topic(topic).Publish(ctx, &pubsub.Message{
+		Data: []byte(fmt.Sprintf("Testing %s", topic)),
+	})
+	fmt.Printf("Published %q: %q\n", topic, res)
+}
+
+func putTopic(ctx context.Context, client *pubsub.Client, topic string) {
+	t := client.Topic(topic)
+	// service account may not have permissions to check exists or create topic
+	ok, err := t.Exists(ctx)
+	if err != nil {
+		log.Fatalf("Failed to check %q exists: %v", topic, err)
+	}
+	if ok {
+		fmt.Println("\nTopic %s already exists!\n", topic)
+		return
+	}
+
+	t, err = client.CreateTopic(ctx, topic)
+	if err != nil {
+		log.Fatalf("\nfailed to create the topic %q: %v", topic, err)
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s key.json\n", path.Base(os.Args[0]))
+		os.Exit(1)
+	}
+	keyPath := os.Args[1]
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", keyPath)
+
+	keyFile, err := ioutil.ReadFile(keyPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var keyData KeyData
+	err = json.Unmarshal(keyFile, &keyData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	projectID := keyData.ProjectID
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to connect to datastore\n")
+	}
+
+	sub := "sports-subscription"
+	topic := "sports-aggregation"
+	//listSubscriptions(ctx, client, topic)
+	//putTopic(ctx, client, topic)
+	putMessage(ctx, client, topic)
+	getMessage(ctx, client, sub)
+}
