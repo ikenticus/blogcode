424b5a8b080d26c40374f2daa61306149caa87c2
ikenticus <ikenticus@gmail.com>
2018-08-28 15:04:18 -0400

    adding hackerrank left-rotation, grouping tasks

---
+++ python/tasks/left-rotation.py
@@ -0,0 +1,37 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    '''
+    # this works
+    left = a[d:]
+    left.extend(a[:d])
+    return left
+    # but it can be made shorter:
+    '''
+    return a[d:] + a[:d]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = raw_input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = map(int, raw_input().rstrip().split())
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
