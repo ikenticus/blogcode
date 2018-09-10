17f55ef22a39b52366df9ae7cd1693fa3ce9dc75
ikenticus <ikenticus@gmail.com>
2018-09-09 23:55:17 -0400

    knocked off another 100+ chars

--- python/tasks/sudoku-solver.py
+++ python/tasks/sudoku-solver.py
@@ -1,45 +1,33 @@
-import random
-T=range(3)
-X=range(9)
-random.shuffle(X)
-def F(g):
+L=range
+Q=L(3)
+X=L(9)
+def E(g):
  for r in X:
   for c in X:
-   if g[r][c]==0:
-    return [r,c]
- return False
+   if g[r][c]==0: return [r,c]
 def B(g,r,c,n):
- for x in T:
-  for y in T:
-   if g[r+x][c+y]==n:
-    return True
- return False
+ for x in Q:
+  for y in Q:
+   if g[r+x][c+y]==n: return 1
 def C(g,c,n):
  for i in X:
-  if g[i][c]==n:
-   return True
- return False
+  if g[i][c]==n: return 1
 def R(g,r,n):
  for i in X:
-  if g[r][i]==n:
-   return True
- return False
+  if g[r][i]==n: return 1
 def S(g,r,c,n):
  return not R(g,r,n) and not C(g,c,n) and not B(g,r-r%3,c-c%3,n)
 def A(g):
- p=F(g)
- if not p:
-  return True
+ p=E(g)
+ if not p: return 1
  r,c=p
- for n in range(1,10):
+ for n in L(1,10):
   if S(g,r,c,n):
    g[r][c]=n
-   if A(g):
-    return g
+   if A(g): return g
    g[r][c]=0
- return False
 n=input()
-for i in range(n):
+for _ in range(n):
  b=[]
  for _ in X:
   b.append([int(k) for k in raw_input().split()])
