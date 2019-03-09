e6a9b8413d2553db39e3b64ef8ae4bdca60eb0b8
Kent Lee <kent@gallerymediagroup.com>
2019-03-09 08:11:38 -0500

    adding move_object to s3 snippet

--- python/snippets/amazon-s3.py
+++ python/snippets/amazon-s3.py
@@ -1,21 +1,36 @@
+import os
+
 import boto3
-from io import StringIO
+s3c = boto3.client('s3')
+s3res = boto3.resource(service_name = 's3')
 
 BUCKET = 'canvas-warehouse'
 
-s3c = boto3.client('s3')
-s3key = 'testing/storage'
-contents = 'Something Blue'
-s3c.put_object(Bucket = BUCKET, Key = s3key, Body = str(contents).encode('utf-8'), ACL = 'public-read')
-print({ 'Success': '%s/%s' % (BUCKET, s3key) })
+def put_object(s3key, contents):
+    s3c.put_object(Bucket=BUCKET, Key=s3key, Body=str(contents).encode('utf-8'), ACL='public-read')
+    return { 'Success': '%s/%s' % (BUCKET, s3key) }
 
-s3res = boto3.resource(service_name = 's3')
-tmpfile = '/tmp/empty'
-w = open(tmpfile, 'w')
-w.write('Empty testing string')
-w.close()
-s3key = 'testing/empty'
-s3res.meta.client.upload_file(Filename = tmpfile, Bucket = BUCKET, Key = s3key, ExtraArgs = {'ACL': 'public-read'})
-#s3obj = s3res.Bucket(BUCKET).Object(s3key)
-#s3obj.Acl().put(ACL='public-read')
-print({ 'Success': '%s/%s' % (BUCKET, s3key) })
+def upload_file(s3key, contents, tmpfile):
+    w = open(tmpfile, 'w')
+    w.write(contents)
+    w.close()
+    s3res.meta.client.upload_file(Filename=tmpfile, Bucket=BUCKET, Key=s3key, ExtraArgs={'ACL': 'public-read'})
+    #s3obj = s3res.Bucket(BUCKET).Object(s3key)
+    #s3obj.Acl().put(ACL='public-read')
+    return { 'Success': '%s/%s' % (BUCKET, s3key) }
+
+def move_objects(source, dest):
+    #listing = s3res.meta.client.list_objects(Bucket=BUCKET, Prefix=source)
+    listing = s3c.list_objects(Bucket=BUCKET, Prefix=source)
+    for obj in listing.get('Contents'):
+        s3key = obj.get('Key')
+        target = s3key.replace(source, dest)
+        print(target)
+        copy_source = {'Bucket': BUCKET, 'Key': s3key}
+        s3c.copy_object(CopySource=copy_source, Bucket=BUCKET, Key=target)
+        s3c.delete_object(Bucket=BUCKET, Key=s3key)
+
+if __name__ == '__main__':
+    print(put_object('testing/storage', 'Something New'))
+    #print(upload_file('testing/storage', 'Something Blue', '/tmp/empty'))
+    move_objects('testing', 'moved')
